8fef87154bfdd464b42561c7759fe2932c17a254
gosha7 <goshashonin@gmail.com>
2023-03-13 14:09:32 +0300

    help panel

--- src/main/java/app/Colors.java
+++ src/main/java/app/Colors.java
@@ -59,6 +59,14 @@ public class Colors {
      */
     public static final int BUTTON_COLOR = Misc.getColor(80, 0, 0, 0);
     /**
+     * Цвет текста
+     */
+    public static final int HELP_TEXT = Misc.getColor(255, 255, 255, 255);
+    /**
+     * Цвет фона
+     */
+    public static final int HELP_TEXT_BACKGROUND = Misc.getColor(50, 0, 0, 0);
+    /**
      * Цвет пересечения
      */
     public static final int CROSSED_COLOR = Misc.getColor(200, 0, 255, 255);
