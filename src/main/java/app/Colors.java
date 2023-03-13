6e8804093114a600dd7c03c38e82d7624af0b316
gosha7 <goshashonin@gmail.com>
2023-03-13 13:28:30 +0300

    cs rendering

--- src/main/java/app/Colors.java
+++ src/main/java/app/Colors.java
@@ -31,6 +31,10 @@ public class Colors {
      */
     public static final int MULTILINE_TEXT_COLOR = Misc.getColor(64, 255, 255, 255);
     /**
+     * Цвет сетки
+     */
+    public static final int TASK_GRID_COLOR = Misc.getColor(64, 255, 255, 255);
+    /**
      * Цвет подложки поля ввода
      */
     public static final int FIELD_BACKGROUND_COLOR = Misc.getColor(255, 255, 255, 255);
